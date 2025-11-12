5f5ca794080004721de9e882df06a2660ac54a8b
Erangamadhushan <erangamadhushann956@gmail.com>
2025-11-12 23:02:10 +0530

    feat: enhance watch controller with CRUD operations and improve response structure

--- routes/watch/watch.routes.js
+++ routes/watch/watch.routes.js
@@ -3,8 +3,17 @@ const router = express.Router();
 
 const watchController = require('../../controllers/watch/watch.controller');
 
-// Route to get all watches
-// base routes: api/watches
+// Route to get all watches, base routes: api/watches
 router.get('/', watchController.getAllWatches);
+
+// Route: /api/watches/:model
+router.get('/:model', watchController.getWatchByModel);
+
+// Create a new watch
+router.post('/create-watch-model', watchController.createWatch);
+
+// Delete a watch by model number
+router.delete('/delete-watch-model/:model', watchController.deleteWatch);
+
 
 module.exports = router;
